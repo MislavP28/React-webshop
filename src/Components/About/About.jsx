98f485dcc807b03c8fccaeac884c1b1b8e34c357
MislavP28 <mihapavicic@gmail.com>
2024-04-08 21:45:01 +0200

    COmit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,34 @@
+
+function About () {
+    return (
+        <main class="onama">
+        <div>
+                    <h2>GPU Kritičar</h2>
+                    <section class="onama_td">
+                        <div>
+                            <p>GpuKritičarShop je Web Shop GPU Kritičar stranice gdje posjetitelji mogu kupiti svu PC opremu koja će im ikad trebati pri izradi svojeg PC setupa. Nudimo razumne cijene za kupce, koje su među najnižima na području Republike Hrvatske. Bile bi i niže, ali iz nekog razloga država ima previsok porez za elektorniku pa to tjera konkurenciju da diže cijene komponenti kao da živimo u UAE, a ne u hrvatskoj. Zato kupujte kod nas a ne kod konkurencije, osim ako nevolite bacati novce bezveze onda kupujte di oćete.
+                            </p>
+                            <p>GPU Kritičar je web stranica napravljena za ljubitelje PC hardwarea i kao pomoć onima koji to tek postaju ili samo traže pomoć kod sastavljanja svog PC-ja. Naš tim se sastoji od 3 člana (nadamo se sa tendencijom rasta, a ne pada što se tiće zaposlenih).</p> 
+                            <p>Svaki član našeg tima je ljubitelj tehnologije i ima potrebno znanje ne samo vezano za PC komponente već za različite druge tehnologije od televizora, mobitela pa sve do pametnih hladnjaka. Naše sjedište se nalazi u Pirovcu.</p>
+                            
+                        </div>
+                    </section>
+        </div>
+        
+        
+        <div class="divonama"> 
+            <section>
+                        <ul>
+                            <li>Glavni urednik: Valsim Ćičivap</li><br>
+                            <li>Pomoćni urednik: Stjepanče Pacov-Tipkovnović</li><br>
+                            <li>Pomoćni urednih: Alenče Oktavijan Smajlović</li>
+                        </ul>
+            </section>
+        </div>
+        <div>
+                     <img src="Slike/images/istockphoto-1133861582-612x612.jpg" alt="Slika"> 
+        </div>
+    </main>
+
+    )
+}
---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,31 @@
+
+function Blog(){
+    return (
+        <div>
+                        <section class="sekšon1">
+                <h2>Nove NVIDIA gpu</h2>
+                
+                    <img class="slika1" src="Slike/slike proizvoda/rtx_4080.jpg" alt="rtx_4080">
+                    <p>NVIDIA je na CES 2024 Predstavila nove varijante svojih grafičkih kartica iz 4000 serije. Nove najavljene Kartice su 4070 Super (599$), 4070 ti Super(799$) i 4080 Super(999$) koje bi se trebale naći na tržištu krajem siječnja. Sve grafičke kartice bi trebala donijeti lagano poboljšanje na svoje osnovne verzije a 4080 Super čak donosi i manju cijenu. Pratite nas pa kada se dočepamo kartica budemo postavili review i benchmark rezultate da sami možete odlučiti koju kupiti. </p>
+                
+            </section>
+            <section class="sekšon2">
+                <h2>Novi QD-OLED monitori</h2>
+              
+                    <img class="slika2" src="Slike/slike proizvoda/samsung-odyssey-oled-g8-g80sd.webp" alt="g8">
+                    <p>Tijekom prosinca Samsung je najavio nova dva monitora za gaming koji koriste QD-OLED tehnologiju veličine 32" i 27". Monitor veličine 32" dolazi u 4k rezoluciji i osvježenjem ekrana 240Hz, a monitor od 27" dolazi sa 2k rezolucijom i osvježenjem ekrana od 360Hz. </p>
+                
+            </section>
+            <section class="sekšon3">
+                <h2>Ryzen 8000 Serija</h2>
+                
+                    <img class="slika3" src="Slike/slike proizvoda/AMD1.jpg" alt="ryzen78700g">
+                    <p>Tijekom siječnja AMD je predstavio novu seriju Ryzen procesora. 8000 serija će se sačinjavati od 4 različita procesora od kojih će 4 biti dostupna za kupnju u maloprodaji. Cijena će se kretati od 175$ do 329$. Novi procesori imaju ceć ugrađenu grafičku u njih te mogu služiti i za igrati igrice.</p>
+
+                
+            </section>
+        </article>
+        </div>
+    )
+}
+export { Blog }
